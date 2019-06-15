b5f354c0a61424d301ecc82e682aef6b89a1cbb7
Martin Hvidberg <martin@hvidberg.net>
2019-06-15 21:52:25 +0200

    TTT secrets seems more random than random :-)

--- tiktaktoe/tiktaktoe/ttt.py
+++ tiktaktoe/tiktaktoe/ttt.py
@@ -1,10 +1,11 @@
-import random
+import secrets
 
 class Suite(object):
 
-    def __init__(self, colour=1):
+    def __init__(self, colour=2):
         if not colour in [1,2]:
-            colour = 1
+            print("err")
+            colour = 2
         self.colour = colour
 
     def flip(self):
@@ -108,17 +109,17 @@ class Rolf_player(object):
         if isinstance(board, TTTboard):
             if not board.full(suite):
                 while True:
-                    guess = random.randrange(9)
+                    guess = secrets.randbelow(9) # random.randrange(9)
                     if board.vacant(guess):
                         board.set(guess, suite)
                         break
             else:
                 while True:
-                    guess1 = random.randrange(9)
+                    guess1 = secrets.randbelow(9) # random.randrange(9)
                     if board.get(guess1) == suite.colour:
                         board.set(guess1, 0)
                         while True:
-                            guess2 = random.randrange(9)
+                            guess2 = secrets.randbelow(9) # random.randrange(9)
                             if guess2 != guess1:
                                 if board.get(guess2) == 0:
                                     board.set(guess2, suite.colour)
@@ -133,20 +134,12 @@ if __name__ == '__main__':
     def play_rolf_x2():
         brd_p = TTTboard()
         rolf = Rolf_player()
-        suite = Suite(random.randrange(2)-1)
+        suite = Suite(secrets.choice([1,2]))
         while not brd_p.done():
             rolf.draw_ttt(brd_p, suite)
         return brd_p.winner()
 
-    brd_tst = TTTboard()
-    rolf = Rolf_player()
-    suite = Suite()
-    while not brd_tst.done():
-        rolf.draw_ttt(brd_tst, suite)
-    print(brd_tst)
-    print("Winner is {}, in {} draws".format(brd_tst.winner(), brd_tst._cnt_draws))
-
     dic_win = {1: 0, 2: 0}
-    for n in range(100000):
+    for n in range(10000):
         dic_win[play_rolf_x2()] += 1
     print(dic_win)
